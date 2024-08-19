847bf6c108083a6d58a16026951efaba212f805b
Wade Hammes <wade.hammes@gmail.com>
2024-08-18 21:21:51 -0400

    Chore: add social images and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,19 +42,19 @@ export default async function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
+          href="/images/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicons/favicon-32x32.png"
+          href="/images/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicons/favicon-16x16.png"
+          href="/images/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
