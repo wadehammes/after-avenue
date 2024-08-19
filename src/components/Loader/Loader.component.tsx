16e7cc8a735ad55cb0ffe4be82c456018dd1a690
Wade Hammes <wadehammes@users.noreply.github.com>
2024-08-18 23:17:28 -0400

    Chore: add loading state (#5)

---
+++ src/components/Loader/Loader.component.tsx
@@ -0,0 +1,10 @@
+import styles from "src/components/Loader/Loader.module.css";
+import AfterAvenueBrandmark from "src/icons/AfterAvenueBrandmark.svg";
+
+export const Loader = () => {
+  return (
+    <div className={styles.loaderContainer}>
+      <AfterAvenueBrandmark />
+    </div>
+  );
+};
