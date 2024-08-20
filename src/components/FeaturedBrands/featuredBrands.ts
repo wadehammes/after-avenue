942686dbf75a65cebc51f9f4932f8612ff5bcd4a
Wade Hammes <wadehammes@users.noreply.github.com>
2024-08-20 14:42:59 -0400

    Update Contentful (#24)

--- src/components/FeaturedBrands/featuredBrands.ts
+++ src/components/FeaturedBrands/featuredBrands.ts
@@ -3,6 +3,7 @@ import Disney from "src/icons/Disney.svg";
 import Dove from "src/icons/Dove.svg";
 import GE from "src/icons/GE.svg";
 import Samsung from "src/icons/Samsung.svg";
+import TheAtlantic from "src/icons/TheAtlantic.svg";
 
 interface FeaturedBrand {
   name: string;
@@ -14,23 +15,31 @@ export const featuredBrands: FeaturedBrand[] = [
   {
     name: "Dove",
     icon: Dove,
+    maxWidth: "clamp(11rem, 5vw, 13rem)",
   },
   {
     name: "Samsung",
     icon: Samsung,
+    maxWidth: "clamp(12rem, 5vw, 14rem)",
   },
   {
     name: "Disney",
     icon: Disney,
+    maxWidth: "clamp(10rem, 5vw, 12rem)",
+  },
+  {
+    name: "The Atlantic",
+    icon: TheAtlantic,
+    maxWidth: "clamp(10rem, 5vw, 12rem)",
   },
   {
     name: "GE",
     icon: GE,
-    maxWidth: "7rem",
+    maxWidth: "clamp(5rem, 5vw, 6rem)",
   },
   {
     name: "Adidas",
     icon: Adidas,
-    maxWidth: "7rem",
+    maxWidth: "clamp(5rem, 5vw, 6rem",
   },
 ];
