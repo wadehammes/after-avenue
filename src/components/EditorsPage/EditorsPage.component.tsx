53c7049047a5755489ae457d42cee046dbf2ca0c
Wade Hammes <wadehammes@users.noreply.github.com>
2024-09-03 01:35:50 -0400

    Chore: fix hydration errors (#31)

--- src/components/EditorsPage/EditorsPage.component.tsx
+++ src/components/EditorsPage/EditorsPage.component.tsx
@@ -7,6 +7,7 @@ import ReactPlayer from "react-player/lazy";
 import styles from "src/components/EditorsPage/EditorsPage.module.css";
 import { Editor } from "src/contentful/getEditors";
 import { Page } from "src/contentful/getPages";
+import { useIsBrowser } from "src/hooks/useIsBrowser";
 
 interface EditorsPageProps {
   pageFields: Page;
@@ -16,6 +17,7 @@ interface EditorsPageProps {
 export const EditorsPage = (props: EditorsPageProps) => {
   const { editors, pageFields } = props;
   const { pageTitle } = pageFields;
+  const isBrowser = useIsBrowser();
   const [currentVideoId, setCurrentVideoId] = useState<string>(
     editors[0].featuredWork?.id ?? "",
   );
@@ -26,55 +28,55 @@ export const EditorsPage = (props: EditorsPageProps) => {
 
   return (
     <>
-      <div className="container column">
-        <h1 className="hidden-title">{pageTitle}</h1>
-        <div className={styles.editorsPageContent}>
-          <div className={styles.editorsList}>
-            {editors.map((editor) => (
-              <Link
-                href={`/editors/${editor.editorSlug}`}
-                className={classNames(styles.editorButton, {
-                  [styles.activeEditor]:
-                    currentVideoId === editor.featuredWork?.id,
-                })}
-                key={editor.editorSlug}
-                type="button"
-                onMouseEnter={() =>
-                  setCurrentVideoId(editor.featuredWork?.id ?? "")
-                }
-              >
-                {editor.editorName}
-              </Link>
-            ))}
-          </div>
+      <h1 className="hidden-title">{pageTitle}</h1>
+      <div className={styles.editorsPageContent}>
+        <div className={styles.editorsList}>
+          {editors.map((editor) => (
+            <Link
+              key={editor.editorSlug}
+              href={`/editors/${editor.editorSlug}`}
+              className={classNames(styles.editorLink, {
+                [styles.activeEditor]:
+                  currentVideoId === editor.featuredWork?.id,
+              })}
+              onMouseEnter={() =>
+                setCurrentVideoId(editor.featuredWork?.id ?? "")
+              }
+            >
+              {editor.editorName}
+            </Link>
+          ))}
         </div>
       </div>
-      <div className={styles.videoBackground}>
-        <div className={styles.overlay} />
-        {editors.map((editor) => {
-          if (!editor.featuredWork) {
-            return null;
-          }
 
-          return (
-            <ReactPlayer
-              key={editor.editorSlug}
-              style={{
-                opacity: currentVideoId === editor.featuredWork?.id ? 1 : 0,
-                transition: "opacity 0.75s ease-in-out",
-                zIndex: currentVideoId === editor.featuredWork?.id ? 1 : 0,
-                position: "absolute",
-                inset: 0,
-              }}
-              controls={false}
-              playing
-              loop
-              muted
-              url={editor.featuredWork.workVideoUrl}
-            />
-          );
-        })}
-      </div>
+      {isBrowser ? (
+        <div className={styles.videoBackground}>
+          <div className={styles.overlay} />
+          {editors.map((editor) => {
+            if (!editor.featuredWork) {
+              return null;
+            }
+
+            return (
+              <ReactPlayer
+                key={editor.editorSlug}
+                style={{
+                  opacity: currentVideoId === editor.featuredWork?.id ? 1 : 0,
+                  transition: "opacity 0.75s ease-in-out",
+                  zIndex: currentVideoId === editor.featuredWork?.id ? 1 : 0,
+                  position: "absolute",
+                  inset: 0,
+                }}
+                controls={false}
+                playing
+                loop
+                muted
+                url={editor.featuredWork.workVideoUrl}
+              />
+            );
+          })}
+        </div>
+      ) : null}
     </>
   );
 };
--- src/contentful/getEditors.ts
+++ src/contentful/getEditors.ts
@@ -65,6 +65,7 @@ export async function fetchAllEditors({
       content_type: "editors",
       include: 10,
       limit: 1000,
+      order: ["fields.editorName"],
     });
 
   return editorResults.items.map(
