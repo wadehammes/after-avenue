1d087abf62b9046c2891b6580da856d504003baf
Wade Hammes <wade.hammes@gmail.com>
2024-10-02 11:55:28 -0400

    Chore: update Biome

--- src/components/StyledButton/StyledButtonLink.component.tsx
+++ src/components/StyledButton/StyledButtonLink.component.tsx
@@ -6,41 +6,37 @@ import type { AriaLinkOptions } from "react-aria";
 import styles from "src/components/StyledButton/StyledButton.module.css";
 
 interface StyledButtonLinkProps extends Omit<AriaLinkOptions, "href"> {
-	children: ReactNode;
-	color?: "light" | "dark";
-	href: Url;
-	inverted?: boolean;
-	size?: "small" | "medium" | "large";
-	variant: "contained" | "outlined";
+  children: ReactNode;
+  color?: "light" | "dark";
+  href: Url;
+  size?: "small" | "medium" | "large";
+  variant: "contained" | "outlined";
 }
 
 export const StyledButtonLink = (props: StyledButtonLinkProps) => {
-	const {
-		children,
-		color = "light",
-		inverted,
-		size = "medium",
-		variant,
-		...restProps
-	} = props;
+  const {
+    children,
+    color = "light",
+    size = "medium",
+    variant,
+    ...restProps
+  } = props;
 
-	return (
-		<Link
-			{...restProps}
-			className={classNames(styles.styledButton, {
-				[styles.light]: color === "light",
-				[styles.contained]: variant === "contained",
-				[styles.outlined]: variant === "outlined",
-				[styles.inverted]: inverted,
-				[styles.dark]: color === "dark",
-				[styles.small]: size === "small",
-				[styles.medium]: size === "medium",
-				[styles.large]: size === "large",
-			})}
-		>
-			{children}
-		</Link>
-	);
+  return (
+    <Link
+      {...restProps}
+      className={classNames(styles.styledButton, {
+        [styles.light]: color === "light",
+        [styles.contained]: variant === "contained",
+        [styles.outlined]: variant === "outlined",
+        [styles.dark]: color === "dark",
+        [styles.small]: size === "small",
+        [styles.large]: size === "large",
+      })}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export default StyledButtonLink;
--- src/lib/generateSitemap.ts
+++ src/lib/generateSitemap.ts
@@ -3,8 +3,8 @@ import fs from "fs";
 const baseUrl = "https://www.afteravenue.com";
 
 export interface SitemapItem {
-	route: string;
-	modTime: string;
+  route: string;
+  modTime: string;
 }
 
 const generateSitemapItem = ({ route, modTime }: SitemapItem): string => `
@@ -18,23 +18,23 @@ const generateSitemapItem = ({ route, modTime }: SitemapItem): string => `
 export const generateSitemap = (routes: SitemapItem[]): string => `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     ${routes
-			.map((route) =>
-				generateSitemapItem({
-					route: route.route,
-					modTime: route.modTime,
-				}),
-			)
-			.join("")}
+      .map((route) =>
+        generateSitemapItem({
+          route: route.route,
+          modTime: route.modTime,
+        }),
+      )
+      .join("")}
 </urlset>
 `;
 
 export const outputSitemap = (routes: SitemapItem[], filename: string) => {
-	if (!routes || !filename) {
-		// eslint-disable-next-line no-console -- displays error
-		return console.error("Missing routes or filename");
-	}
+  if (!routes || !filename) {
+    // eslint-disable-next-line no-console -- displays error
+    return console.error("Missing routes or filename");
+  }
 
-	const sitemap = generateSitemap(routes);
+  const sitemap = generateSitemap(routes);
 
-	fs.writeFileSync(`./public/generated-sitemap-${filename}.xml`, sitemap);
+  fs.writeFileSync(`./public/generated-sitemap-${filename}.xml`, sitemap);
 };
